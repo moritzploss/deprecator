4f44af5299323641d199a27953c64f5d5404dec5
Moritz Ploss <moritz.ploss@kivra.com>
2022-04-10 11:01:58 +0200

    improve api

---
+++ duration.go
@@ -0,0 +1,38 @@
+package deprecator
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+)
+
+// see https://stackoverflow.com/questions/48050945/how-to-unmarshal-json-into-durations
+
+type Duration struct {
+	time.Duration
+}
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		d.Duration = time.Duration(value)
+		return nil
+	case string:
+		var err error
+		d.Duration, err = time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		return errors.New("invalid duration")
+	}
+}
